d516e410d3046571fe0c29eb683c773b3f631cef
your.cavalier <your.cavalier@gmail.com>
2017-10-23 23:12:03 -0400

    Init commit - add files, dirs.

---
+++ index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<?php
+  chdir("video");
+  $d = dir(getcwd());
+
+  echo "Handle: " . $d->handle . "<br>";
+  echo "Path: " . $d->path . "<br>";
+
+  while (($file = $d->read()) !== false){ 
+    echo "filename: " . $file . "<br>"; 
+  } 
+  $d->close();
+?> 
+
+</body>
+</html>
