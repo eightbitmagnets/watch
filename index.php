9c6d1453a46323fd8a8453cbeab45466586d0279
your.cavalier <your.cavalier@gmail.com>
2017-10-25 15:31:25 -0400

    Fixing sorting of directory read.

--- index.php
+++ index.php
@@ -5,23 +5,30 @@
   <link rel="stylesheet" href="styles.css">
   <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
   <script>
+  var videoFileName;
   function loadVid(videoFileName) {
     $('.container').append('<video width="100%" height="auto" controls><source src="' + videoFileName + '" type="video/mp4"></video>');
   };
   </script>
   <?php
     function writeVidList() {
+      $fileArray = array();
       $videoDir = opendir('video');
       while (($file = readdir($videoDir)) !== false) {
         if ($file != '.' && $file != '..') {
+          
           $removeExt = explode('.mp4',$file);
           $removeSpaces = explode(' ',$file);
           $extRemoved = implode('',$removeExt);
-          $addHtmlSpaces = implode('%20',$removeSpaces);
-          echo '<li onclick="loadVid(' . $file . ')">' . $extRemoved . "</li>\n\t";
+          
+          array_push($fileArray,$file);
         }
       }
-    }f
+      sort($fileArray);
+      for ($x = 0; $x <= count($fileArray); $x++) {
+        echo '<li onclick="loadVid(' . $fileArray . ')">' . $fileArray[$x] . "</li>\n\t";
+      }
+    }
   ?>
 </head>
 <body>
