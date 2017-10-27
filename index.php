84181b1cd606daf6f54a9ac056c3c3a006f0545f
your.cavalier <your.cavalier@gmail.com>
2017-10-27 15:36:52 -0400

    Working to use mostly JS to write stuff, and PHP to get file info from server.

--- index.php
+++ index.php
@@ -1,39 +1,50 @@
 <!DOCTYPE html>
 <html>
+
 <head>
   <title>Watch</title>
   <link rel="stylesheet" href="styles.css">
   <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
   <script>
-  var videoFileName;
-  function loadVid(videoFileName) {
-    $('.container').append('<video width="100%" height="auto" controls><source src="' + videoFileName + '" type="video/mp4"></video>');
-  };
-  </script>
-  <?php
     function writeVidList() {
-      $fileArray = array();
-      $videoDir = opendir('video');
-      while (($file = readdir($videoDir)) !== false) {
-        if ($file != '.' && $file != '..') {
-          
-          $removeExt = explode('.mp4',$file);
-          $removeSpaces = explode(' ',$file);
-          $extRemoved = implode('',$removeExt);
-          
-          array_push($fileArray,$file);
-        }
-      }
-      sort($fileArray);
-      for ($x = 0; $x <= count($fileArray); $x++) {
-        echo '<li onclick="loadVid(' . $fileArray . ')">' . $fileArray[$x] . "</li>\n\t";
-      }
-    }
-  ?>
+      var vidList = [
+             <?php
+              function makeVidList() {
+                $fileArray = array();
+                $videoDir = opendir('video');
+                while (($file = readdir($videoDir)) !== false) {
+                  if ($file != '.' && $file != '..') {
+                    array_push($fileArray,$file);
+                  }
+                }
+                sort($fileArray);
+                $x = 0;
+                foreach ($fileArray as $value) {
+                  echo $fileArray[$x] . ', ';
+                  $x ++;
+                }
+              }
+            ?>];
+      
+      var sortedArray = <?php $fileArray ?>;
+      
+      $('ul').append('<li onclick="loadVid()"></li>');
+      console.log(sortedArray);
+    };
+    
+    function loadVid(arg) {
+      var vidURL = encodeURIComponent(arg);
+      $('.container').append('<video width="100%" height="auto" controls><source src="' + vidURL + '" type="video/mp4"></video>');
+    };
+  </script>
 </head>
+
 <body>
   <div class="container"></div>
   <ul>
-    <?php writeVidList() ?></ul>
+    <?php makeVidList() ?>
+    <script>writeVidList();</script>
+  </ul>
 </body>
+
 </html>
